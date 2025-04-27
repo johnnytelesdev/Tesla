fd2144695e8aeb07f6d0af3b2e9b75f7ae50a3cf
Johnny Teles Gil <141787979+johnnytelesdev@users.noreply.github.com>
2025-04-27 17:52:03 -0300

    Site Tesla
    Projeto inicial Tesla

---
+++ Site Tesla/scripts.js
@@ -0,0 +1,21 @@
+alert("teste tesla")
+
+//variaveis ==> uma forma de identificar quem é quem , dar um nome pra um pedacinho de momoria para guardar uma informação!
+// como se criase uma pasta para guardar um informação!
+
+
+
+// funções é um trecho de código que executa só quando é chamado!
+ //documente é igual a ir no html e queryselector busca quem nos queremos
+
+let formulario = document.querySelector(".formulario")
+
+function aparecerFormulario(){
+    formulario.style.left = "30%"
+
+}
+
+
+function desaparecerFormulario(){
+formulario.style.left = "-40%"
+}
